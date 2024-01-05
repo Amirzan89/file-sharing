2e8fd582c0f4f7426546c7719abc6921925fe36b
Amirzan Fikri Prasetyo <102281081+Amirzan89@users.noreply.github.com>
2024-01-05 19:00:37 +0700

    update progress area

--- resources/views/upload1.blade.php
+++ resources/views/upload1.blade.php
@@ -10,6 +10,11 @@
     <title>upload 1</title>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
     @vite('resources/css/app.css')
+    <style>
+        .progress-area::-webkit-scrollbar{
+            width: 0px;
+        }
+    </style>
 </head>
 <body class="max-h-screen select-none">
     <div> 
@@ -23,7 +28,7 @@
             <input type="file" name="myFiles" class="filepond file-input" multiple hidden>
         </form>
         <div class="mt-12 h-2/4 w-5/6 mx-auto overflow-hidden flex flex-col text-black">
-            <ul class="progress-area bg-red-500 h-2/4 mb-3 flex flex-col gap-5 overflow-hidden">
+            <ul class="progress-area  h-2/4 mb-3 flex flex-col gap-2 overflow-y-scroll scrollbar-none scroll-behavior-smooth">
                 <li class="row relative  border-1 bg-white rounded-xl">
                     <i class="fas fa-file-alt text-3xl relative left-3 top-2/4 -translate-y-2/4"></i>
                     <div class="details absolute top-1 left-12">
@@ -44,58 +49,64 @@
                         <i class="fa-solid fa-x cursor-pointer"></i>
                     </div>
                 </li>
-                <li class="row bg-blue-500">
-                    <div class="content">
-                        <i class="fas fa-file-alt"></i>
-                        <div class="details">
-                            <span class="name">image_01.png+ Uploading</span>
+                <li class="row relative  border-1 bg-white rounded-xl">
+                    <i class="fas fa-file-alt text-3xl relative left-3 top-2/4 -translate-y-2/4"></i>
+                    <div class="details absolute top-1 left-12">
+                        <span class="name">image_01.png+ Uploading</span>
+                        <div class="size-and-percent">
                             <span class="size">4MB</span>
-                            <span class="percent">50%</span>
-                        </div>
-                        <div class="progress-bar">
-                            <div class="progress"></div>
-                        </div>
-                        <div class="controls">
-                            <i class="fa-solid fa-play cursor-pointer"></i>
-                            <i class="fa-solid fa-pause cursor-pointer"></i>
-                            <i class="fa-solid fa-x cursor-pointer"></i>
                         </div>
                     </div>
-                </li>
-                <li class="row">
-                    <div class="content">
-                        <i class="fas fa-file-alt"></i>
-                        <div class="details">
-                            <span class="name">image_01.png+ Uploading</span>
-                            <span class="size">4MB</span>
-                            <span class="percent">50%</span>
+                    <div class="mt-3 mb-1 ml-12  flex flex-row items-center gap-2">
+                        <div class="progress-bar w-5/6 h-3 bg-blue-500/20 rounded-xl">
+                            <div class="progress bg-blue-500 w-3/4 h-3"></div>
                         </div>
-                        <div class="progress-bar">
-                            <div class="progress"></div>
-                        </div>
-                        <div class="controls">
-                            <i class="fa-solid fa-play cursor-pointer"></i>
-                            <i class="fa-solid fa-pause cursor-pointer"></i>
-                            <i class="fa-solid fa-x cursor-pointer"></i>
-                        </div>
+                        <span class="percent">50%</span>
+                    </div>
+                    <div class="controls absolute right-4 top-4 text-xl flex flex-row gap-2">
+                        <i class="fa-solid fa-play cursor-pointer"></i>
+                        <i class="fa-solid fa-pause cursor-pointer"></i>
+                        <i class="fa-solid fa-x cursor-pointer"></i>
                     </div>
                 </li>
-                <li class="row">
-                    <div class="content">
-                        <i class="fas fa-file-alt"></i>
-                        <div class="details">
-                            <span class="name">terserah</span>
+                <li class="row relative  border-1 bg-white rounded-xl">
+                    <i class="fas fa-file-alt text-3xl relative left-3 top-2/4 -translate-y-2/4"></i>
+                    <div class="details absolute top-1 left-12">
+                        <span class="name">image_01.png+ Uploading</span>
+                        <div class="size-and-percent">
                             <span class="size">4MB</span>
-                            <span class="percent">100%</span>
                         </div>
-                        <div class="progress-bar">
-                            <div class="progress" style="width:${percent}%;"></div>
+                    </div>
+                    <div class="mt-3 mb-1 ml-12  flex flex-row items-center gap-2">
+                        <div class="progress-bar w-5/6 h-3 bg-blue-500/20 rounded-xl">
+                            <div class="progress bg-blue-500 w-3/4 h-3"></div>
                         </div>
-                        <div class="controls">
-                            <i class="fa-solid fa-play cursor-pointer"></i>
-                            <i class="fa-solid fa-pause cursor-pointer"></i>
-                            <i class="fa-solid fa-x cursor-pointer"></i>
+                        <span class="percent">50%</span>
+                    </div>
+                    <div class="controls absolute right-4 top-4 text-xl flex flex-row gap-2">
+                        <i class="fa-solid fa-play cursor-pointer"></i>
+                        <i class="fa-solid fa-pause cursor-pointer"></i>
+                        <i class="fa-solid fa-x cursor-pointer"></i>
+                    </div>
+                </li>
+                <li class="row relative  border-1 bg-white rounded-xl">
+                    <i class="fas fa-file-alt text-3xl relative left-3 top-2/4 -translate-y-2/4"></i>
+                    <div class="details absolute top-1 left-12">
+                        <span class="name">image_01.png+ Uploading</span>
+                        <div class="size-and-percent">
+                            <span class="size">4MB</span>
                         </div>
+                    </div>
+                    <div class="mt-3 mb-1 ml-12  flex flex-row items-center gap-2">
+                        <div class="progress-bar w-5/6 h-3 bg-blue-500/20 rounded-xl">
+                            <div class="progress bg-blue-500 w-3/4 h-3"></div>
+                        </div>
+                        <span class="percent">50%</span>
+                    </div>
+                    <div class="controls absolute right-4 top-4 text-xl flex flex-row gap-2">
+                        <i class="fa-solid fa-play cursor-pointer"></i>
+                        <i class="fa-solid fa-pause cursor-pointer"></i>
+                        <i class="fa-solid fa-x cursor-pointer"></i>
                     </div>
                 </li>
             </ul>
