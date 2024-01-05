5d558312d6b324e243af90ba9015813b083e448a
Amirzan Fikri Prasetyo <102281081+Amirzan89@users.noreply.github.com>
2024-01-05 21:18:26 +0700

    update uploaded area

--- resources/views/upload1.blade.php
+++ resources/views/upload1.blade.php
@@ -11,7 +11,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"/>
     @vite('resources/css/app.css')
     <style>
-        .progress-area::-webkit-scrollbar{
+        .progress-area::-webkit-scrollbar,
+        .uploaded-area::-webkit-scrollbar{
             width: 0px;
         }
     </style>
@@ -28,14 +29,12 @@
             <input type="file" name="myFiles" class="filepond file-input" multiple hidden>
         </form>
         <div class="mt-12 h-2/4 w-5/6 mx-auto overflow-hidden flex flex-col text-black">
-            <ul class="progress-area  h-2/4 mb-3 flex flex-col gap-2 overflow-y-scroll scrollbar-none scroll-behavior-smooth">
-                <li class="row relative  border-1 bg-white rounded-xl">
+            <ul class="progress-area h-2/4 mb-3 flex flex-col gap-2 overflow-y-scroll scrollbar-none scroll-behavior-smooth">
+                <li class="row relative bg-white rounded-xl">
                     <i class="fas fa-file-alt text-3xl relative left-3 top-2/4 -translate-y-2/4"></i>
                     <div class="details absolute top-1 left-12">
                         <span class="name">image_01.png+ Uploading</span>
-                        <div class="size-and-percent">
-                            <span class="size">4MB</span>
-                        </div>
+                        <div class="size">4MB</div>
                     </div>
                     <div class="mt-3 mb-1 ml-12  flex flex-row items-center gap-2">
                         <div class="progress-bar w-5/6 h-3 bg-blue-500/20 rounded-xl">
@@ -43,19 +42,17 @@
                         </div>
                         <span class="percent">50%</span>
                     </div>
-                    <div class="controls absolute right-4 top-4 text-xl flex flex-row gap-2">
+                    <div class="controls absolute right-4 top-4 text-xl flex gap-2">
                         <i class="fa-solid fa-play cursor-pointer"></i>
                         <i class="fa-solid fa-pause cursor-pointer"></i>
                         <i class="fa-solid fa-x cursor-pointer"></i>
                     </div>
                 </li>
-                <li class="row relative  border-1 bg-white rounded-xl">
+                <li class="row relative bg-white rounded-xl">
                     <i class="fas fa-file-alt text-3xl relative left-3 top-2/4 -translate-y-2/4"></i>
                     <div class="details absolute top-1 left-12">
                         <span class="name">image_01.png+ Uploading</span>
-                        <div class="size-and-percent">
-                            <span class="size">4MB</span>
-                        </div>
+                        <div class="size">4MB</div>
                     </div>
                     <div class="mt-3 mb-1 ml-12  flex flex-row items-center gap-2">
                         <div class="progress-bar w-5/6 h-3 bg-blue-500/20 rounded-xl">
@@ -63,19 +60,17 @@
                         </div>
                         <span class="percent">50%</span>
                     </div>
-                    <div class="controls absolute right-4 top-4 text-xl flex flex-row gap-2">
+                    <div class="controls absolute right-4 top-4 text-xl flex gap-2">
                         <i class="fa-solid fa-play cursor-pointer"></i>
                         <i class="fa-solid fa-pause cursor-pointer"></i>
                         <i class="fa-solid fa-x cursor-pointer"></i>
                     </div>
                 </li>
-                <li class="row relative  border-1 bg-white rounded-xl">
+                <li class="row relative bg-white rounded-xl">
                     <i class="fas fa-file-alt text-3xl relative left-3 top-2/4 -translate-y-2/4"></i>
                     <div class="details absolute top-1 left-12">
                         <span class="name">image_01.png+ Uploading</span>
-                        <div class="size-and-percent">
-                            <span class="size">4MB</span>
-                        </div>
+                        <div class="size">4MB</div>
                     </div>
                     <div class="mt-3 mb-1 ml-12  flex flex-row items-center gap-2">
                         <div class="progress-bar w-5/6 h-3 bg-blue-500/20 rounded-xl">
@@ -83,19 +78,17 @@
                         </div>
                         <span class="percent">50%</span>
                     </div>
-                    <div class="controls absolute right-4 top-4 text-xl flex flex-row gap-2">
+                    <div class="controls absolute right-4 top-4 text-xl flex gap-2">
                         <i class="fa-solid fa-play cursor-pointer"></i>
                         <i class="fa-solid fa-pause cursor-pointer"></i>
                         <i class="fa-solid fa-x cursor-pointer"></i>
                     </div>
                 </li>
-                <li class="row relative  border-1 bg-white rounded-xl">
+                <li class="row relative bg-white rounded-xl">
                     <i class="fas fa-file-alt text-3xl relative left-3 top-2/4 -translate-y-2/4"></i>
                     <div class="details absolute top-1 left-12">
                         <span class="name">image_01.png+ Uploading</span>
-                        <div class="size-and-percent">
-                            <span class="size">4MB</span>
-                        </div>
+                        <div class="size">4MB</div>
                     </div>
                     <div class="mt-3 mb-1 ml-12  flex flex-row items-center gap-2">
                         <div class="progress-bar w-5/6 h-3 bg-blue-500/20 rounded-xl">
@@ -103,60 +96,111 @@
                         </div>
                         <span class="percent">50%</span>
                     </div>
-                    <div class="controls absolute right-4 top-4 text-xl flex flex-row gap-2">
+                    <div class="controls absolute right-4 top-4 text-xl flex gap-2">
                         <i class="fa-solid fa-play cursor-pointer"></i>
                         <i class="fa-solid fa-pause cursor-pointer"></i>
                         <i class="fa-solid fa-x cursor-pointer"></i>
                     </div>
                 </li>
             </ul>
-            <ul class="uploaded-area bg-green-500 h-2/4 mt-3">
-                <li class="row">
-                    <div class="content">
-                        <i class="fas fa-file-alt"></i>
-                        <div class="details">
-                            <span class="name">image_01 + Uploaded</span>
-                            <span class="size">70KB</span>
-                        </div>
-                        <div class="controls">
-                            <i class="fas fa-check"></i>
-                        </div>
+            <ul class="uploaded-area h-2/4 mt-3 flex flex-col gap-2 overflow-y-scroll scrollbar-none scroll-behavior-smooth">
+                <li class="row relative bg-white rounded-xl flex flex-row min-h-15 items-center">
+                    <i class="fas fa-file-alt text-3xl absolute left-5"></i>
+                    <div class="details absolute  left-15">
+                        <span class="name">image_01 + Uploaded</span>
+                        <div class="size">70KB</div>
+                    </div>
+                    <div class="controls absolute right-3 text-2xl flex gap-3">
+                        <i class="fas fa-check"></i>
+                        <i class="fa-solid fa-trash"></i>
                     </div>
                 </li>
-                <li class="row">
-                    <div class="content">
-                        <i class="fas fa-file-alt"></i>
-                        <div class="details">
-                            <span class="name">image_01 + Uploaded</span>
-                            <span class="size">70KB</span>
-                        </div>
-                        <div class="controls">
-                            <i class="fas fa-check"></i>
-                        </div>
+                <li class="row relative bg-white rounded-xl flex flex-row min-h-15 items-center">
+                    <i class="fas fa-file-alt text-3xl absolute left-5"></i>
+                    <div class="details absolute  left-15">
+                        <span class="name">image_01 + Uploaded</span>
+                        <div class="size">70KB</div>
+                    </div>
+                    <div class="controls absolute right-3 text-2xl flex gap-3">
+                        <i class="fas fa-check"></i>
+                        <i class="fa-solid fa-trash"></i>
                     </div>
                 </li>
-                <li class="row">
-                    <div class="content">
-                        <i class="fas fa-file-alt"></i>
-                        <div class="details">
-                            <span class="name">image_01 + Uploaded</span>
-                            <span class="size">70KB</span>
-                        </div>
-                        <div class="controls">
-                            <i class="fas fa-check"></i>
-                        </div>
+                <li class="row relative bg-white rounded-xl flex flex-row min-h-15 items-center">
+                    <i class="fas fa-file-alt text-3xl absolute left-5"></i>
+                    <div class="details absolute  left-15">
+                        <span class="name">image_01 + Uploaded</span>
+                        <div class="size">70KB</div>
+                    </div>
+                    <div class="controls absolute right-3 text-2xl flex gap-3">
+                        <i class="fas fa-check"></i>
+                        <i class="fa-solid fa-trash"></i>
                     </div>
                 </li>
-                <li class="row">
-                    <div class="content">
-                        <i class="fas fa-file-alt"></i>
-                        <div class="details">
-                            <span class="name">image_01 + Uploaded</span>
-                            <span class="size">70KB</span>
-                        </div>
-                        <div class="controls">
-                            <i class="fas fa-check"></i>
-                        </div>
+                <li class="row relative bg-white rounded-xl flex flex-row min-h-15 items-center">
+                    <i class="fas fa-file-alt text-3xl absolute left-5"></i>
+                    <div class="details absolute  left-15">
+                        <span class="name">image_01 + Uploaded</span>
+                        <div class="size">70KB</div>
+                    </div>
+                    <div class="controls absolute right-3 text-2xl flex gap-3">
+                        <i class="fas fa-check"></i>
+                        <i class="fa-solid fa-trash"></i>
+                    </div>
+                </li>
+                <li class="row relative bg-white rounded-xl flex flex-row min-h-15 items-center">
+                    <i class="fas fa-file-alt text-3xl absolute left-5"></i>
+                    <div class="details absolute  left-15">
+                        <span class="name">image_01 + Uploaded</span>
+                        <div class="size">70KB</div>
+                    </div>
+                    <div class="controls absolute right-3 text-2xl flex gap-3">
+                        <i class="fas fa-check"></i>
+                        <i class="fa-solid fa-trash"></i>
+                    </div>
+                </li>
+                <li class="row relative bg-white rounded-xl flex flex-row min-h-15 items-center">
+                    <i class="fas fa-file-alt text-3xl absolute left-5"></i>
+                    <div class="details absolute  left-15">
+                        <span class="name">image_01 + Uploaded</span>
+                        <div class="size">70KB</div>
+                    </div>
+                    <div class="controls absolute right-3 text-2xl flex gap-3">
+                        <i class="fas fa-check"></i>
+                        <i class="fa-solid fa-trash"></i>
+                    </div>
+                </li>
+                <li class="row relative bg-white rounded-xl flex flex-row min-h-15 items-center">
+                    <i class="fas fa-file-alt text-3xl absolute left-5"></i>
+                    <div class="details absolute  left-15">
+                        <span class="name">image_01 + Uploaded</span>
+                        <div class="size">70KB</div>
+                    </div>
+                    <div class="controls absolute right-3 text-2xl flex gap-3">
+                        <i class="fas fa-check"></i>
+                        <i class="fa-solid fa-trash"></i>
+                    </div>
+                </li>
+                <li class="row relative bg-white rounded-xl flex flex-row min-h-15 items-center">
+                    <i class="fas fa-file-alt text-3xl absolute left-5"></i>
+                    <div class="details absolute  left-15">
+                        <span class="name">image_01 + Uploaded</span>
+                        <div class="size">70KB</div>
+                    </div>
+                    <div class="controls absolute right-3 text-2xl flex gap-3">
+                        <i class="fas fa-check"></i>
+                        <i class="fa-solid fa-trash"></i>
+                    </div>
+                </li>
+                <li class="row relative bg-white rounded-xl flex flex-row min-h-15 items-center">
+                    <i class="fas fa-file-alt text-3xl absolute left-5"></i>
+                    <div class="details absolute  left-15">
+                        <span class="name">image_01 + Uploaded</span>
+                        <div class="size">70KB</div>
+                    </div>
+                    <div class="controls absolute right-3 text-2xl flex gap-3">
+                        <i class="fas fa-check"></i>
+                        <i class="fa-solid fa-trash"></i>
                     </div>
                 </li>
             </ul>
