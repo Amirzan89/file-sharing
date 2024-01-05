3e3f64e0af69c3ab79a06bbf0b6ae89fb5ee61bb
Amirzan Fikri Prasetyo <102281081+Amirzan89@users.noreply.github.com>
2024-01-05 22:14:03 +0700

    add pre component vue

--- resources/js/Components/upload1.vue
+++ resources/js/Components/upload1.vue
@@ -1,12 +1,32 @@
 <template>
-    
+    <header class="text-black text-4xl text-center mt-7 font-medium">File Uploader</header>
+    <form method="POST" action="/users/upload" enctype="multipart/form-data" class="w-5/6 h-1/4 mt-10 mx-auto flex flex-col items-center justify-center text-black border-blue-500 border-3 border-dashed rounded-2xl gap-4 cursor-pointer">
+        <i class="fa-solid fa-images text-5xl"></i>
+        <span class="text-2xl">Choose a file or drag it here</span>
+        <input type="file" name="myFiles" class="filepond file-input" multiple hidden>
+    </form>
+    <div class="mt-12 h-2/4 w-5/6 mx-auto overflow-hidden flex flex-col text-black">
+        <ul class="progress-area h-2/4 mb-3 flex flex-col gap-2 overflow-y-scroll scrollbar-none scroll-behavior-smooth">
+        </ul>
+        <ul class="uploaded-area h-2/4 mt-3 flex flex-col gap-2 overflow-y-scroll scrollbar-none scroll-behavior-smooth"></ul>
+    </div>
 </template>
 <script>
+import progressComponent from './upload1/progress.vue';
+import uploadComponent from './upload1/uploaded.vue';
 export default{
+    components:{
+        progressComponent:progressComponent,
+        uploadComponent:uploadComponent
+    },
     data(){
         return{
             index:0,
-
+        }
+    },
+    methods:{
+        upload(){
+            //
         }
     }
 }
---
+++ resources/js/Components/upload1/progress.vue
@@ -0,0 +1,33 @@
+<template>
+    <li class="row relative bg-white rounded-xl">
+        <i class="fas fa-file-alt text-3xl relative left-3 top-2/4 -translate-y-2/4"></i>
+        <div class="details absolute top-1 left-12">
+            <span class="name">image_01.png+ Uploading</span>
+            <div class="size">4MB</div>
+        </div>
+        <div class="mt-3 mb-1 ml-12  flex flex-row items-center gap-2">
+            <div class="progress-bar w-5/6 h-3 bg-blue-500/20 rounded-xl">
+                <div class="progress bg-blue-500 w-3/4 h-3"></div>
+            </div>
+            <span class="percent">50%</span>
+        </div>
+        <div class="controls absolute right-4 top-4 text-xl flex gap-2">
+            <i class="fa-solid fa-play cursor-pointer"></i>
+            <i class="fa-solid fa-pause cursor-pointer"></i>
+            <i class="fa-solid fa-x cursor-pointer"></i>
+        </div>
+    </li>
+</template>
+<script>
+export default{
+    props:[''],
+    data(){
+        return{
+            //
+        }
+    },
+    methods:{
+        
+    }
+}
+</script>
